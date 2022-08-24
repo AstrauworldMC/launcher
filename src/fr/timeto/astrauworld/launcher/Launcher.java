1042311fa30aee3581752da13071692d4bc566fd
TimEtOff <timeogodefroy@gmail.com>
2022-08-25 00:58:23 +0200

    Revert "Seconde mise en place des classes principales"
    This reverts commit a68e2e1d4ffc96b6c4b9069eb8479ec6be239e6d.

--- src/fr/timeto/astrauworld/launcher/Launcher.java
+++ src/fr/timeto/astrauworld/launcher/Launcher.java
@@ -1,18 +1,6 @@
 package fr.timeto.astrauworld.launcher;
 
-import fr.flowarg.flowlogger.ILogger;
-import fr.flowarg.flowlogger.Logger;
-import fr.flowarg.flowupdater.FlowUpdater;
-import fr.flowarg.flowupdater.download.DownloadList;
-import fr.flowarg.flowupdater.download.IProgressCallback;
-import fr.flowarg.flowupdater.download.Step;
-import fr.flowarg.flowupdater.download.json.CurseFileInfo;
-import fr.flowarg.flowupdater.download.json.Mod;
-import fr.flowarg.flowupdater.versions.AbstractForgeVersion;
-import fr.flowarg.flowupdater.versions.ForgeVersionBuilder;
-import fr.flowarg.flowupdater.versions.VanillaVersion;
 import fr.flowarg.openlauncherlib.NewForgeVersionDiscriminator;
-import fr.flowarg.openlauncherlib.NoFramework;
 import fr.litarvan.openauth.microsoft.MicrosoftAuthResult;
 import fr.litarvan.openauth.microsoft.MicrosoftAuthenticationException;
 import fr.litarvan.openauth.microsoft.MicrosoftAuthenticator;
@@ -22,9 +10,6 @@ import fr.theshark34.openlauncherlib.util.CrashReporter;
 import java.io.File;
 import java.nio.file.Path;
 import java.nio.file.Paths;
-import java.text.DecimalFormat;
-import java.util.ArrayList;
-import java.util.List;
 
 public class Launcher {
 
@@ -43,7 +28,7 @@ public class Launcher {
 
     // Version de Minecraft et de Forge utilisée
     static final String mcVersion = "1.18.2";
-    static final String forgeVersion = "40.1.73";
+    static final String forgeVerion = "40.1.73";
 
     // Version du launcher
     public static final String version = "BETA2.0.0";
@@ -67,7 +52,7 @@ public class Launcher {
     public static final Path awSecondProfileData = Paths.get(secondProfileData);
     public static final Path awThirdProfileData = Paths.get(thirdProfileData);
 
-    public static final GameInfos AW_INFOS = new GameInfos("Astrauworld", awGameFilesFolder, new GameVersion(mcVersion, GameType.V1_13_HIGHER_FORGE.setNFVD(new NewForgeVersionDiscriminator(forgeVersion, mcVersion, "20211210.034407"))), new GameTweak[] {GameTweak.FORGE});
+    public static final GameInfos AW_INFOS = new GameInfos("Astrauworld", awGameFilesFolder, new GameVersion(mcVersion, GameType.V1_13_HIGHER_FORGE.setNFVD(new NewForgeVersionDiscriminator(forgeVerion, mcVersion, "20211210.034407"))), new GameTweak[] {GameTweak.FORGE});
 
     private static AuthInfos authInfos;
     private static Thread updateThread;
@@ -85,105 +70,7 @@ public class Launcher {
     }
 
     public static void launch() throws Exception{
-        NoFramework noFramework = new NoFramework(awGameFilesFolder, authInfos, GameFolder.FLOW_UPDATER);
-        // noFramework.getAddtionalArgs().addAll(Arrays.asList("--server", "207.180.196.61", "--port", "33542"));
 
-        LauncherFrame.getInstance().setVisible(false);
-
-        noFramework.launch(mcVersion, forgeVersion);
-        System.exit(0);
-    }
-
-    public enum StepInfo {
-
-        INTEGRATION("Chargement de l’integration..."),
-        MOD_PACK("Téléchargement du pack de mods..."),
-        DL_LIBS("Téléchargement des librairies..."),
-        DL_ASSETS("Téléchargement des assets..."),
-        EXTRACT_NATIVES("Extraction des natives..."),
-        FORGE("Installation de Forge..."),
-        FABRIC("Installation de Fabric..."),
-        MODS("Téléchargement des mods..."),
-        EXTERNAL_FILES("Téléchargement des fichiers externes..."),
-        POST_EXECUTIONS("Post-executions..."),
-        END("Terminé!");
-
-        String details;
-
-        StepInfo(String details) {
-            this.details = details;
-        }
-
-        public String getDetails() {
-            return details;
-        }
-
-    }
-
-    public static void update() throws Exception {
-        Logger logger = new Logger("[Astrauworld Launcher]", awLogsFile);
-
-        IProgressCallback callback = new IProgressCallback() {
-            private final DecimalFormat decimalFormat = new DecimalFormat("#.#");
-
-            @Override
-            public void init(ILogger logger) {}
-
-            @Override
-            public void step(Step step) {
-                LauncherPanel.infoLabel.setText(StepInfo.valueOf(step.name()).getDetails());
-            }
-
-            public void onFileDownloads(Path path) {
-                LauncherPanel.fileLabel.setEtxt(path.getFileName().toString());
-            }
-
-            @Override
-            public void update(DownloadList.DownloadInfo info) {
-                int progress = (int) info.getDownloadedBytes();
-                int maximum = (int) info.getTotalToDownloadBytes();
-
-                LauncherPanel.percentLabel.setText(decimalFormat.format((progress / maximum) * 100) + "%");
-                LauncherPanel.progressBar.setValue(progress);
-                LauncherPanel.progressNar.setMaximum(maximum);
-            }
-        };
-
-        final VanillaVersion vanillaVersion = new VanillaVersion.VanillaVersionBuilder()
-                .withName(mcVersion)
-                .build();
-
-        final List<CurseFileInfo> modInfos = new ArrayList<>();
-
-        final List<Mod> mods = new ArrayList<>();
-
-        final AbstractForgeVersion forge = new ForgeVersionBuilder(ForgeVersionBuilder.ForgeVersionType.NEW)
-                .withForgeVersion(mcVersion + "-" + forgeVersion)
-                .withCurseMods(modInfos)
-                .withMods(mods)
-                .build();
-
-        final FlowUpdater updater = new FlowUpdater.FlowUpdaterBuilder()
-                .withVanillaVersion(vanillaVersion)
-                .withLogger(logger)
-                .withProgressCallback(callback)
-                .withModLoaderVersion(forge)
-                .build();
-        updater.update(awGameFilesFolder);
-    }
-
-    public static void installOtherFiles(String wantedResources) {
-        final FlowUpdater installer = new FlowUpdater.FlowUpdaterBuilder()
-                .build();
-        // installer.update();
-    }
-
-    public static void interruptThread() {
-        updateThread.interrupt();
-    }
-
-    public static CrashReporter getCrashReporter() {
-        return crashReporter;
     }
 
 }
